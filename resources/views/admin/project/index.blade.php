2e37325f324b0b6cd530d1045a5c55964bbaf45e
albertobaggio96 <albertobaggio96@gmail.com>
2023-03-01 16:57:17 +0100

    print to index e show technologies

--- resources/views/admin/project/index.blade.php
+++ resources/views/admin/project/index.blade.php
@@ -27,6 +27,7 @@
         <th scope="col">date</th>
         <th scope="col">preview</th>
         <th scope="col">type</th>
+        <th scope="col">technologies</th>
         <th scope="col">option</th>
       </tr>
     </thead>
@@ -39,6 +40,11 @@
             <td>{{ $project->date }}</td>
             <td><img src="{{ filter_var($project->preview, FILTER_VALIDATE_URL)  ? $project->preview : asset('storage/'. $project->preview)}}" alt="{{ $project->title }}" class="preview"></td>
             <td>{{ $project->type->type }}</td>
+            <td>
+              @foreach ($project->technologies as $technology)
+                {{ $technology->technology }}
+              @endforeach
+            </td>
             <td>
               <a href="{{ route("admin.projects.show", $project->slug) }}" class="btn btn-show px-4"><i class="fa-solid fa-eye"></i></a>
               <a href="{{ route("admin.projects.edit", $project->slug) }}" class="btn btn-edit px-4"><i class="fa-solid fa-pen-to-square"></i></a>
--- resources/views/admin/project/show.blade.php
+++ resources/views/admin/project/show.blade.php
@@ -12,6 +12,12 @@
       <img src="{{ filter_var($project->preview, FILTER_VALIDATE_URL)  ? $project->preview : asset('storage/'. $project->preview)}}" alt="{{ $project->title }}" class="preview">
 
       <h1>{{ $project->title }}</h1>
+      <h3>
+        Technoligies:
+        @foreach($project->technologies as $technology)
+          {{ $technology->technology }}
+        @endforeach
+      </h3>
       <h3>Author: {{ $project->author }}</h3>
       <div>date: {{ $project->date }}</div>
       <div class="d-flex mt-2">
