66c58b098c0f46770a7535b93b89c09d521c9be1
albertobaggio96 <albertobaggio96@gmail.com>
2023-03-01 15:14:24 +0100

    fix name of file

--- database/migrations/2023_03_01_140146_create_technologies_table.php
+++ database/migrations/2023_03_01_140146_create_technologies_table.php
@@ -13,9 +13,9 @@ return new class extends Migration
      */
     public function up()
     {
-        Schema::create('tecnologies', function (Blueprint $table) {
+        Schema::create('technologies', function (Blueprint $table) {
             $table->id();
-            $table->string('technology', 10);
+            $table->string('technology');
             $table->timestamps();
         });
     }
@@ -27,6 +27,6 @@ return new class extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('tecnologies');
+        Schema::dropIfExists('technologies');
     }
 };
--- database/migrations/2023_03_01_141106_create_project_technology_table.php
+++ database/migrations/2023_03_01_141106_create_project_technology_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('project_technology', function (Blueprint $table) {
+            
             $table->unsignedBigInteger('project_id');
             $table->foreign('project_id')->references('id')->on('projects');
 
